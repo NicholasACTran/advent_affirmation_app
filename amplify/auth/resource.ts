cd702d76f622acfc93c645813b04ae2bb4636bdb
NicholasACTran <nicholas.ac.tran@gmail.com>
2025-10-09 10:35:16 -0400

    Updated amplify.yml and reverted import changes

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,15 +1,6 @@
 import { defineAuth } from '@aws-amplify/backend';
 import { postConfirmation } from './post-confirmation/resource';
 
-import { Amplify } from "aws-amplify";
-import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-import { env } from "$amplify/env/post-confirmation";
-
-const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(
-  env
-);
-
-Amplify.configure(resourceConfig, libraryOptions);
 
 /**
  * Define and configure your auth resource
