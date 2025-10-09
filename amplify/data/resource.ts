791da1ef9d5203aaefbc9c14174596b6f5324cfa
NicholasACTran <nicholas.ac.tran@gmail.com>
2025-10-09 12:17:17 -0400

    Moved postConfirmation function to functions folder

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-import { postConfirmation } from "../auth/post-confirmation/resource";
+import { postConfirmation } from "../functions/post-confirmation/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
